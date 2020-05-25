4a49e40fd4cef5f27c1f6b8fef269a9ad574730e
Liubomyyr <liubko81@gmail.com>
2020-05-25 22:22:40 +0300

    camera complete

--- game/Assets/Scripts/Player/cameraFollow/CameraController.cs
+++ game/Assets/Scripts/Player/cameraFollow/CameraController.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 using System;
 public class CameraController : MonoBehaviour
@@ -9,20 +8,29 @@ public class CameraController : MonoBehaviour
     protected virtual void SetUp(Func<Vector3> cameraFollowPositionFunc)  {
         this.cameraFollowPositionFunc = cameraFollowPositionFunc;
     }
+
     protected virtual void UIInteract() { } 
     void Update()
     {
-        Application.targetFrameRate = 5;
-
         Vector3 cameraFollowPosition = cameraFollowPositionFunc();
         cameraFollowPosition.z = transform.position.z;
 
-        Vector3 cameraMoveDir = cameraFollowPosition - transform.position.normalized;
+        Vector3 cameraMoveDir = (cameraFollowPosition - transform.position).normalized;
         float distance = Vector3.Distance(cameraFollowPosition, transform.position);
         float cameraMoveSpeed = 1f;
 
-        transform.position = transform.position + cameraMoveDir * distance * cameraMoveSpeed * Time.deltaTime;
+        if(distance > 0)
+        {
+            Vector3 newCameraPositiontransform = transform.position + cameraMoveDir * distance * cameraMoveSpeed * Time.deltaTime;
 
-            UIInteract();          
+            float distanceAfterMoving = Vector3.Distance(newCameraPositiontransform, cameraFollowPosition);
+
+            if (distanceAfterMoving > distance) {
+                newCameraPositiontransform = cameraFollowPosition;
+            }
+
+            transform.position = newCameraPositiontransform;
+        }
+        UIInteract();          
     }
 }
