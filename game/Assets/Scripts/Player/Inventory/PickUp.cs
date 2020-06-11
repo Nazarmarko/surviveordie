b17523893dfe58893ec59b9a1ec2af2783776d31
Liubomyyr <liubko81@gmail.com>
2020-06-11 12:12:23 +0300

    new Inventory UI

---
+++ game/Assets/Scripts/Player/Inventory/PickUp.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    private Inventory inventory;
+
+    public GameObject itemButtom;
+    private void OnEnable()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Inventory").GetComponent<Inventory>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D invetoryCol)
+    {
+        if (invetoryCol.CompareTag("Inventory"))
+        {
+            for (int i = 0; i < inventory.slotInInventory.Length; i++)
+            {
+                if (inventory.isFull[i] == false)
+                {
+                    inventory.isFull[i] = true;
+
+                    Instantiate(itemButtom, inventory.slotInInventory[i].transform);
+
+                    Destroy(gameObject);
+                    break;
+                }
+            }
+        }
+    }
+}
